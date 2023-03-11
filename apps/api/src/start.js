c7cbd13b847b5d4aabc7902d689fd3c9436e9674
roblesdotdev <httpsnip@gmial.com>
2023-03-11 17:23:24 -0300

    api/setup database with models

--- apps/api/src/start.js
+++ apps/api/src/start.js
@@ -3,11 +3,19 @@ if (process.env.NODE_ENV === "development") {
 }
 const logger = require("loglevel");
 const server = require("./server");
+const { conn } = require("./db");
 
 const APP_PORT = process.env.APP_PORT || 3001;
+const DB_SYNC = process.env.DB_SYNC || true;
 
 logger.setLevel(process.env.NODE_ENV !== "production" ? "TRACE" : "SILENT");
 
 server.listen(APP_PORT, async () => {
-  logger.info(`Server listening at ${server.address().port}`);
+  logger.info(`✅ Server listening at ${server.address().port}`);
+  try {
+    await conn.sync({ force: DB_SYNC });
+    logger.info("✅ Database is up");
+  } catch (err) {
+    logger.error(`❌ ${err.message}`);
+  }
 });
