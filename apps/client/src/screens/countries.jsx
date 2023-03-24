bfa72ca9e636a72642b03f26689fd973ffa20010
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-03-24 17:43:15 -0300

    Client/scss lib (#2)

--- apps/client/src/screens/countries.jsx
+++ apps/client/src/screens/countries.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 
 export default function CountriesScreen() {
   return (
-    <div>
-      <h1>Countries</h1>
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <h1 className="text-xl">Countries</h1>
       <Link to="BRA">Brasil</Link>
     </div>
   )
--- apps/client/src/screens/welcome.jsx
+++ apps/client/src/screens/welcome.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 
 export default function WelcomeScreen() {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <h1 className="text-xl">Countries</h1>
       <Link to="dash">Explore</Link>
     </div>
   )
